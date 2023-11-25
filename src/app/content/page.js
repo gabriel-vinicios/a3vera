7c17ccb97b33fdf614c402d8bd54f9c840a5e798
Gabriel Vinicios <crisishd2332@gmail.com>
2023-11-24 21:34:19 -0300

    hotfix

--- src/app/content/page.js
+++ src/app/content/page.js
@@ -8,7 +8,7 @@ export default function Content() {
       <div className="mx-auto max-w-2xl lg:mx-0">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Content</h2>
         <p className="mt-2 text-lg leading-8 text-gray-600">
-          Learn how to grow your business with our expert advice.
+          Learn how to use AI in your teaching!
         </p>
       </div>
       <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
